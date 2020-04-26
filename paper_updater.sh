2d79569ea74e947049999dcb8e2a8e6d9a8c3c31
VinterNix <44822382+VinterNix@users.noreply.github.com>
2020-04-26 15:36:04 -0400

    Auto PaperMC Updater

---
+++ paper_updater.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Thanks to msniveau for all the help with this
+
+# Setup our temp working directory
+WORKDIR=$(mkdir -d)
+
+# Download most latest 1.15.2 release of papermc
+wget -P $WORKDIR/ https://papermc.io/api/v1/paper/1.15.2/latest/download
+
+# Identify the update file
+UPDATE_FILE=$(ls $WORKDIR/paper-*.jar)
+[[ "$?" -gt 0 ]] && echo "paper-*.jar not found, exiting!" && exit 1
+
+# Fetch the new md5sum
+NEW_VERSION=$(md5sum $UPDATE_FILE | awk '{print $1}')
+
+# Now iterate over the servers, each server needs to check if an update is required
+for i in /home/pufferd/servers/*/paperspigot.jar; do #CHANGE TO YOUR DIR
+    SERVER_DIR=$(dirname $i)
+    # Compare currently installed m5sum with the new one
+    [[ "$($SERVER_DIR/paperspigot.jar | awk '{print $1}')" == "$NEW_VERSION" ]] && continue
+    echo "$SERVER_DIR needs update!"
+    cp $UPDATE_FILE $SERVER_DIR/paperspigot.jar
+    chown pufferd:pufferd $SERVER_DIR/paperspigot.jar
+fi
+
+# Delete our temp directory
+rm -rf $WORKDIR
