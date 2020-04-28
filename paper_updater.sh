7216fbd3a6c4b726a037c1b96ad5abe3efbc226e
VinterNix <44822382+VinterNix@users.noreply.github.com>
2020-04-28 16:43:37 -0400

    correcting md5 check, few other little mods

--- paper_updater.sh
+++ paper_updater.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Thanks to msniveau for all the help with this
+# Update Line 19 with your mc server path
 
-# Setup our temp working directory
+# Define our workspace
 WORKDIR=$(mktemp -d)
 
 # Download most latest 1.15.2 release of papermc
-wget -P $WORKDIR/ https://papermc.io/api/v1/paper/1.15.2/latest/download/ -O paperspigot.jar
+wget -O $WORKDIR/paperspigot.jar https://papermc.io/api/v1/paper/1.15.2/latest/download
 
 # Identify the update file
 UPDATE_FILE=$(ls $WORKDIR/paperspigot.jar)
@@ -15,14 +16,14 @@ UPDATE_FILE=$(ls $WORKDIR/paperspigot.jar)
 NEW_VERSION=$(md5sum $UPDATE_FILE | awk '{print $1}')
 
 # Now iterate over the servers, each server needs to check if an update is required
-for i in /home/pufferd/servers/*/paperspigot.jar; do #CHANGE TO YOUR DIR
+for i in /INSERT/FULL/PATH/TO/MINECRAFT/SERVER/HERE; do
     SERVER_DIR=$(dirname $i)
     # Compare currently installed m5sum with the new one
-    [[ "$($SERVER_DIR/paperspigot.jar | awk '{print $1}')" == "$NEW_VERSION" ]] && continue
+    [[ "$(md5sum $SERVER_DIR/paperspigot.jar | awk '{print $1}')" == "$NEW_VERSION" ]] && continue
     echo "$SERVER_DIR needs update!"
     cp $UPDATE_FILE $SERVER_DIR/paperspigot.jar
     chown pufferd:pufferd $SERVER_DIR/paperspigot.jar
-fi
+done
 
-# Delete our temp directory
+# Cleanup our workspace
 rm -rf $WORKDIR
